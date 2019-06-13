cbce73d52e7c386d74fbc96817ae461b7083b17e
Sanjaya <you@example.com>
2019-06-13 09:15:03 +0530

    [002]-Missing traning data would be downloaded from the original tesseract project. If the auto downloaded file produces errors, download and place the file manually.

--- tesseract-ocr/src/main/java/main/CMDMain.java
+++ tesseract-ocr/src/main/java/main/CMDMain.java
@@ -3,10 +3,18 @@
  */
 package main;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.itextpdf.text.pdf.codec.Base64.OutputStream;
 
 import file.FileUtil;
 import net.sourceforge.tess4j.Tesseract;
@@ -33,10 +41,39 @@ public class CMDMain {
 
 	public static void main(final String[] args) throws FileNotFoundException, TesseractException, IOException {
 		final String imagesFolder = su.concat(".", File.separator, "workspace", File.separator, "images");
+		// Check and download training data if not already present.
+		downloadTrainingData();
 		// Read list of files.
 		final File[] files = FileUtil.readNonDirectoriesByDate(imagesFolder);
 		// perform OCR on all files.
 		ocr(files);
+	}
+
+	private static void downloadTrainingData() throws IOException {
+		String tesseractDir = pm.get(null, "tesseractDir");
+		String ocrLanguage = pm.get(null, "ocrLanguage");
+		String trainedData = su.concat(ocrLanguage, ".traineddata");
+		String remoteLocation = su.concat(pm.get(null, "tesserractTrainedDataDownloadLocation"), "/", trainedData);
+		String fileToBeChecked = su.concat(tesseractDir, File.separator, trainedData);
+		if (!new File(fileToBeChecked).exists()) {
+			// Trained data not found, download it now.
+			System.out.println(su.concat("Training data not found for [", fileToBeChecked + "], downloading from [",
+					remoteLocation, "]"));
+			URL url = new URL(remoteLocation);
+			URLConnection urlConn = url.openConnection();
+			byte[] buffer = new byte[4096]; // declare 4KB buffer
+			int len;
+
+			try (FileOutputStream fos = new FileOutputStream(fileToBeChecked);
+					BufferedOutputStream bos = new BufferedOutputStream(fos);
+					InputStream inputStream = urlConn.getInputStream();) {
+				while ((len = inputStream.read(buffer)) > 0) {
+					bos.write(buffer, 0, len);
+				}
+				bos.flush();
+			}
+			System.out.println("Download complete!");
+		}
 	}
 
 	private static void ocr(final File[] files) throws TesseractException, FileNotFoundException, IOException {
