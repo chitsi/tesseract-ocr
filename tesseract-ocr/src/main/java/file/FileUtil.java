4c31fef95236c427c0739ebd53984d95ba2b47b5
Sanjaya <you@example.com>
2020-08-03 02:33:13 +0530

    File name sequence would be based on Java implementation. This is alphabetical on Windows.

--- tesseract-ocr/src/main/java/file/FileUtil.java
+++ tesseract-ocr/src/main/java/file/FileUtil.java
@@ -28,14 +28,14 @@ public class FileUtil {
 	 */
 	public static File[] readNonDirectoriesByDate(String inputFolder) {
 		final File[] listFiles = new File(inputFolder).listFiles(new FilesFileFilter());
-		Map<File, File> map = new TreeMap<File, File>(new FileDateComparator());
-		for (File file : listFiles) {
-			map.put(file, file);
-		}
-		int index = 0;
-		for (Entry<File, File> entry : map.entrySet()) {
-			listFiles[index++] = entry.getValue();
-		}
+//		Map<File, File> map = new TreeMap<File, File>(new FileDateComparator());
+//		for (File file : listFiles) {
+//			map.put(file, file);
+//		}
+//		int index = 0;
+//		for (Entry<File, File> entry : map.entrySet()) {
+//			listFiles[index++] = entry.getValue();
+//		}
 		return listFiles;
 	}
 
